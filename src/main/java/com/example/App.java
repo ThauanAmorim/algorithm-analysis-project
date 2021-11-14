feb43ca2a68cd7f4bcdff71c3229e0b79072aab8
Thauan Amorim <thauanssussu@gmail.com>
2021-11-14 03:22:35 -0300

    ordered and almost ordered generators has be improved

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,12 +7,10 @@ import com.example.utils.ArrayGenerator;
 
 public class App {
     public static void main( String[] args ) {
-        Integer[] array = null;
         ArrayGenerator arrayGenerator = new ArrayGenerator();
 
-        array = arrayGenerator.ordered(1000000);
-        for (Integer integer : array) {
-            System.out.println(integer);
-        }
+        arrayGenerator.almostOrdered(10, 70);
+        
+        System.out.println(arrayGenerator.toString());
     }
 }
--- src/main/java/com/example/utils/ArrayGenerator.java
+++ src/main/java/com/example/utils/ArrayGenerator.java
@@ -1,13 +1,20 @@
 package com.example.utils;
 
+import java.util.Random;
+
 public class ArrayGenerator {
     private Integer[] array = null;
+    private Random random = new Random();
 
     public Integer[] ordered(int quantity) {
         array = new Integer[quantity];
+        int count = 2;
+        int lastNumber = 0;
 
-        for(int i = 0; i< quantity; i++) {
-            array[i] = i;
+        for(int i = 0; i < quantity; i++) {
+            array[i] = random.nextInt(count) + (lastNumber + 1);
+            lastNumber = array[i];
+            count++;
         }
         return array;
     }
@@ -20,5 +27,61 @@ public class ArrayGenerator {
             array[index++] = i;
         }
         return array;
+    }
+
+    public Integer[] almostOrdered(int quantity, float orderedPorcent) {
+        array = ordered(quantity);
+
+        int exchangesNumber = (int) (quantity - (quantity * (orderedPorcent / 100)));
+        int firstPosition;
+        int secondPosition;
+        int aux;
+
+        System.out.println(toString());
+        if(exchangesNumber % 2 != 0) {
+            --exchangesNumber;
+
+            int index = 0;
+            do {
+                index = random.nextInt(quantity);
+            } while (array[index]+1 == array[index+1] || index == 0);
+            
+            if(index == quantity-1 || index == quantity-1) {
+                aux = array[index-1];
+                array[index-1] = array[index];
+                array[index] = aux;
+            } else {
+                int number = 0;
+                do {
+                    number = random.nextInt((array[index+2] - array[index+1] - 1)) + (array[index+1] + 1);
+                } while (number == array[index]);
+                array[index] = number;
+            }
+        }
+
+        if(exchangesNumber <= 0)
+            return array;
+
+        for(int i = 0; i < exchangesNumber / 2; i++) {
+            firstPosition = random.nextInt(quantity);
+            do {
+                secondPosition = random.nextInt(quantity);
+
+            } while (secondPosition == firstPosition);
+
+            aux = array[firstPosition];
+            array[firstPosition] = array[secondPosition];
+            array[secondPosition] = aux;
+        }
+        return array;
+    }
+
+    public String toString() {
+        String text = "[";
+        for (int i = 0; i < array.length-1; i++) {
+            text += array[i] + ", ";
+        }
+        text += array[array.length-1] + "]";
+        return text;
     }
 }
